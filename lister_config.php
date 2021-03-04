951c9639e5f36c7fd362be111b9d07528190f5e0
guillaumelondon <79899552+guillaumelondon@users.noreply.github.com>
2021-03-04 10:46:57 +0000

    Added lister_config.php
    configuration file added

---
+++ lister_config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Config file to change
+
+$subject = "MYSERVER ".php_uname ('n'); // target server
+$install_dir = "/root/lister";  // install dir where everything happens
+$target_dir = '/var/www';       // target directory
+$mode = 1; // 1 = new/mod 2 = new only
+$run = 1; // 1 = run lister , 0 = you need to configure lister
+
+?>
