3c0781df567d9b1473c7d3d249f0ef818a98e56c
guillaumelondon <79899552+guillaumelondon@users.noreply.github.com>
2021-03-08 11:16:44 +0000

    Update lister_config.php
    changed so that people must edit config file.

--- lister_config.php
+++ lister_config.php
@@ -6,6 +6,6 @@ $subject = "MYSERVER ".php_uname ('n'); // target server
 $install_dir = "/root/lister";  // install dir where everything happens
 $target_dir = '/var/www';       // target directory
 $mode = 1; // 1 = new/mod 2 = new only
-$run = 1; // 1 = run lister , 0 = you need to configure lister
+$run = 0; // 1 = run lister , 0 = you need to configure lister
 
 ?>
